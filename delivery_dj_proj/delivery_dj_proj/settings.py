c8a5482b6b64b380f8f7e1f7870e128b28476e4f
Wassim El Moussaoui <wae17@mail.aub.edu>
2021-03-19 19:11:44 +0200

    Testing

--- delivery_dj_proj/delivery_dj_proj/settings.py
+++ delivery_dj_proj/delivery_dj_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1l2dr(8zf2z3_z%ixq8wrr9%do!l6t7&(*71$9a(7p!e1^fn(w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['7bc54ac38e57.ngrok.io',  '127.0.0.1']
+ALLOWED_HOSTS = ['a84a794b3db6.ngrok.io',  '127.0.0.1']
 
 
 # Application definition
