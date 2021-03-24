949ef0c0596ebbdc6fc0b77a063360662272880e
Roy Araman <rja54@mail.aub.edu>
2021-03-24 17:57:55 +0200

    done feature

--- delivery_dj_proj/orders/views.py
+++ delivery_dj_proj/orders/views.py
@@ -9,11 +9,18 @@ from rest_framework.response import Response
 
 
 @api_view(['GET'])
-def send_orders(request,driver_id):
+def send_orders_driver(request,driver_id):
     if request.method == 'GET':
         orders = Driver.objects.get(pk=driver_id).order_set.filter(is_done=False)
         dictionaries = [order.as_dict() for order in orders]
         return JsonResponse(dictionaries,safe=False)        
+
+@api_view(['GET'])
+def send_orders_merchant(request,merch_id):
+    if request.method == 'GET':
+        orders = Merchant.objects.get(pk=merch_id).order_set.filter(is_done=False)
+        dictionaries = [order.as_dict() for order in orders]
+        return JsonResponse(dictionaries,safe=False)      
 
 
 @api_view(['POST'])
