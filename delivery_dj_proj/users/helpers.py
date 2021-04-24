7320edebdeadad5eb3b51665766466cf9ad06579
Wassim El Moussaoui <wassim.moussawi001@gmail.com>
2021-04-24 16:51:39 +0300

    Added delete driver and moved helper functions to a seperate file.

---
+++ delivery_dj_proj/users/helpers.py
@@ -0,0 +1,54 @@
+from .models import User, UnauthDriver
+from .models import generateOTP
+import environ
+from twilio.rest import Client
+
+
+
+
+env = environ.Env()
+environ.Env.read_env()
+twilio_client = Client(env('TWILIO_ACCOUNT_SID'), env('TWILIO_AUTH_TOKEN'))
+
+def get_user_by_phone(phone_number):
+    return User.objects.get(phone_number=phone_number)
+
+
+
+def resendSMS(unauth_driver): # helper function to resend sms
+    newToken = generateOTP()
+    UnauthDriver.objects.filter(phone_number=unauth_driver.phone_number)\
+        .update(token=newToken)
+    unauth_driver.token = newToken
+    sendSMS(unauth_driver)
+
+
+
+def check_unauthdriver(phone_nb): # helper function to find if unauth driver exists
+    unauthdriver = UnauthDriver.objects.filter(phone_number=phone_nb)
+    dictionaries = [driver.as_dict() for driver in unauthdriver]
+    if len(dictionaries) == 1:
+        return unauthdriver
+    else:
+        return False        
+
+
+
+def check_authdriver(phone_nb): # helper function to find if auth driver exists
+    authdriver = User.objects.filter(phone_number=phone_nb, is_driver=1)
+    dictionaries = [driver.as_dict() for driver in authdriver]
+    if len(dictionaries) == 1:
+        return authdriver
+    else:
+        return False 
+
+
+
+def sendSMS(unauth_driver): # helper function to send sms
+    message = twilio_client.messages \
+            .create(
+                body="\nHello " + unauth_driver.first_name + " " + unauth_driver.last_name + \
+                ", here is your sign up token: " + unauth_driver.token,
+                from_="+12244343836",
+                to="+961" + unauth_driver.phone_number
+            )
